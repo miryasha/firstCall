2efd66cc9075fa8d9d384b7b76b4b8e9b45a195e
cat-coder <cat-coder>
2022-02-28 00:31:24 -0600

    DF

--- src/schemas/symbol_tbl.sql
+++ src/schemas/symbol_tbl.sql
@@ -1,12 +1,28 @@
-CREATE TABLE  ${table_name} --symbol_timeframe
+CREATE TABLE IF NOT EXISTS ${table_name<<GLD>>}_tbl --table_name is equeal with symbol
 (
 	`{table_name}_id` int NOT NULL AUTO_INCREMENT,
 	`symbol` varchar(20) NOT NULL,
-    `symbol_date` varchar(20) NOT NULL UNIQUE,
+    `symbol_called_date` varchar(20) NOT NULL UNIQUE,
     `opening`  varchar(20) NOT NULL,
     `high` varchar(20) NOT NULL,
     `low`  varchar(20) NOT NULL ,
     `closing`   varchar(20) NOT NULL,
                                         
 	PRIMARY KEY (`{table_name}_id`)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS SPY
+(
+	SPY_id int NOT NULL AUTO_INCREMENT,
+	symbol varchar(20) NOT NULL,
+    symbol_called_date varchar(20) NOT NULL UNIQUE,
+    opening  varchar(20) NOT NULL,
+    high varchar(20) NOT NULL,
+    low  varchar(20) NOT NULL ,
+    closing   varchar(20) NOT NULL,
+                                        
+	PRIMARY KEY (SPY_id)
 );
